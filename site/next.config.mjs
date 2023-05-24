a4e9754940651c5e8379fd8de1fbbc0366ffd2e4
Travis Arnold <travis.arnold@vercel.com>
2023-05-24 08:27:24 -0700

    moar

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 export default {
   transpilePackages: ['design-system'],
+  modularizeImports: {
+    'design-system/components': {
+      transform: 'design-system/components/{{ kebabCase member }}',
+      skipDefaultConversion: true,
+    },
+  },
 }
